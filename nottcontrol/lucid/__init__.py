635ef4e643aff11788fdf603019c2d7245f8cabd
Thomas MAttheussen <thomas.mattheussen@kuleuven.be>
2026-04-28 11:06:43 +0200

    Config.ini change tied to previous commit.

--- nottcontrol/lucid/__init__.py
+++ nottcontrol/lucid/__init__.py
@@ -8,7 +8,3 @@ Created on Wed Dec 17 17:13:16 2025
 from pathlib import Path
 import os
 from nottcontrol.config import Config
-
-_parent = Path(__file__).parent
-_config_path_lucid = os.path.join(_parent, "cfg/config.cfg")
-config_lucid = Config(_config_path_lucid)
